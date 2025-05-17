d94088b227af4817bdfe7d459b0614ff13097381
carrotman189 <shreyas.thota.1@gmail.com>
2025-05-17 01:36:25 -0400

    Wrote code for TestOdo and updated the OdoPods mechanism file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/OdoPods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/OdoPods.java
@@ -3,17 +3,38 @@ package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestOdo;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class OdoPods extends QQMechanism{
-    GoBildaPinpointDriver odo;
+    GoBildaPinpointDriver leftOdoPod;
+    GoBildaPinpointDriver rightOdoPod;
     @Override
     public void init(HardwareMap hardwareMap) {
-        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+        leftOdoPod = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+        rightOdoPod = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
     }
-    public Pose2D getPose(){
-        return odo.getPosition();
+    public Pose2D getLeftPose(){
+        return leftOdoPod.getPosition();
     }
-    public void setPose(Pose2D newPose){
-        odo.setPosition(newPose);
+    public void setLeftPose(Pose2D newPose){
+        leftOdoPod.setPosition(newPose);
+    }
+    public Pose2D getRightPose(){
+        return rightOdoPod.getPosition();
+    }
+    public void setRightPose(Pose2D newPose){
+        rightOdoPod.setPosition(newPose);
+    }
+    @Override
+    public List<QQTest> getTests() {
+
+        return Arrays.asList(
+                new TestOdo("leftOdo", leftOdoPod,leftOdoPod.getPosition()),
+                new TestOdo("rightOdo", rightOdoPod,rightOdoPod.getPosition())
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Tests/TestOdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Tests/TestOdo.java
@@ -1,14 +1,29 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Tests;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.ftc16072.Mechanisms.GoBildaPinpointDriver;
 
-public class TestOdo extends QQTest{
-    TestOdo(String name) {
+
+public class TestOdo extends QQTest {
+    GoBildaPinpointDriver testOdo;
+    Pose2D testPose;
+
+    public TestOdo(String name, GoBildaPinpointDriver odo, Pose2D testPose) {
         super(name);
+        testOdo = odo;
+        this.testPose = testPose;
     }
 
     @Override
     public void run(Telemetry telemetry, boolean on) {
-
+        testOdo.setPosition(testPose);
+        Pose2D currentPose = testOdo.getPosition();
+        telemetry.addData("Odo X", currentPose.getX(DistanceUnit.MM));
+        telemetry.addData("Odo Y", currentPose.getY(DistanceUnit.MM));
+        telemetry.addData("Odo Heading", currentPose.getHeading(AngleUnit.RADIANS));
+        telemetry.update();
     }
 }

0b4c9c7ad99279fc4439ee7d053c9874cba9344c
Alan Smith <alan@randomsmiths.com>
2025-06-11 20:23:41 -0400

    Add pedro pathing

---
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -0,0 +1,46 @@
+package pedroPathing.constants;
+
+import com.pedropathing.localization.Localizers;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.util.CustomFilteredPIDFCoefficients;
+import com.pedropathing.util.CustomPIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class FConstants {
+    static {
+        FollowerConstants.localizers = Localizers.PINPOINT;
+
+        FollowerConstants.leftFrontMotorName = "front_left_motor";
+        FollowerConstants.leftRearMotorName = "back_left_motor";
+        FollowerConstants.rightFrontMotorName = "front_right_motor";
+        FollowerConstants.rightRearMotorName = "back_right_motor";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+
+        FollowerConstants.mass = 5;
+
+        FollowerConstants.xMovement = 57.8741;
+        FollowerConstants.yMovement = 52.295;
+
+        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
+        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(2,0,0.1,0);
+
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
+
+        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
+        FollowerConstants.centripetalScaling = 0.0005;
+
+        FollowerConstants.pathEndTimeoutConstraint = 500;
+        FollowerConstants.pathEndTValueConstraint = 0.995;
+        FollowerConstants.pathEndVelocityConstraint = 0.1;
+        FollowerConstants.pathEndTranslationalConstraint = 0.1;
+        FollowerConstants.pathEndHeadingConstraint = 0.007;
+    }
+}

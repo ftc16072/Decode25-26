afea4881fe4f0ee4eb0b1d9d05d29a15ffd490ef
Keerthana <keerthana.g.thirukonda@gmail.com>
2025-06-23 20:21:51 -0400

    finished making pivot mech code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Pivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Pivot.java
@@ -1,17 +1,13 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestMotor;
-
-import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
-import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestOdo;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestTouchSensor;
 
 import java.util.Arrays;
@@ -21,7 +17,6 @@ public class Pivot extends QQMechanism{
     DcMotor pivot;
     TouchSensor limitSwitch;
     int desiredPosition = 0;
-    int offset;
     final double TICKS_PER_MOTOR_ROTATION = 751.8; // for 223 RPM motor from GoBilda website
     final double TICKS_PER_SHAFT_ROTATION = 28 * TICKS_PER_MOTOR_ROTATION;
 
@@ -29,21 +24,21 @@ public class Pivot extends QQMechanism{
     public void init(HardwareMap hardwareMap) {
         pivot = hardwareMap.get(DcMotor.class, "pivot");
         limitSwitch = hardwareMap.get(TouchSensor.class, "pivot limit switch");
+        pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pivot.setDirection(DcMotorSimple.Direction.REVERSE);
     }
     @Override
     public List<QQTest> getTests(){
         return Arrays.asList(
-                new TestMotor("pivot",pivot,0.5),
+                new TestMotor("pivot up",pivot,0.5),
+                new TestMotor("pivot down",pivot,-0.5),
+
                 new TestTouchSensor("switch",limitSwitch)
         );
     }
     public void moveToSwitch() {
-        if (limitSwitch.isPressed()) {
-            offset = pivot.getCurrentPosition();
-            desiredPosition = offset;
-        }else{
-            desiredPosition = offset - 100;
-        }
+        desiredPosition = (int)-TICKS_PER_SHAFT_ROTATION;
     }
     public void setDestinationAngleDegrees(double angle){
         double rotations = angle/360;
@@ -52,16 +47,28 @@ public class Pivot extends QQMechanism{
 
     @Override
     public void update(Telemetry telemetry) {
-        double kP = .1;
+        double kP = .0005;
         super.update(telemetry);
-        int currentPosition = pivot.getCurrentPosition() - offset;
+
+        if(limitSwitch.isPressed()  && desiredPosition <= 0){
+            pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            desiredPosition = 0;
+        }
+        int currentPosition = pivot.getCurrentPosition();
         int error = desiredPosition - currentPosition;
         double power = kP * error;
+        if (Math.abs(error) < 50){
+            power = 0;
+        }
         pivot.setPower(power);
+        telemetry.addData("power",power);
+        telemetry.addData("current_pos", currentPosition);
+        telemetry.addData("desrired", desiredPosition);
     }
 
     public void stop (){
-        desiredPosition = pivot.getCurrentPosition() - offset;
+        desiredPosition = pivot.getCurrentPosition();
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Opmodes/TestPivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Opmodes/TestPivot.java
@@ -2,21 +2,24 @@ package org.firstinspires.ftc.teamcode.ftc16072.Opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@SuppressWarnings("unused")
 @TeleOp
 public class TestPivot extends QQOpmode {
     public void loop() {
         super.loop();
-        if (gamepad1.a) {
-            robot.pivot.moveToSwitch();
-        } else {
-            robot.pivot.stop();
-        }
+
         if (gamepad1.dpad_down) {
             robot.pivot.setDestinationAngleDegrees(0);
         } else if (gamepad1.dpad_right){
             robot.pivot.setDestinationAngleDegrees(90);
         } else if (gamepad1.dpad_up){
             robot.pivot.setDestinationAngleDegrees(180);
+        }
+        if(gamepad1.a){
+            robot.pivot.moveToSwitch();
+        }
+        if (gamepad1.b) {
+           robot.pivot.stop();
         }
     }
 }

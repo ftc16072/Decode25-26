5823dc9956f0d79d7d53af214e0e3aa581e38bf8
carrotman189 <shreyas.thota.1@gmail.com>
2025-09-20 18:08:58 -0400

    Deleted loki game code, and then created code for ri5w intake and limit switch.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Opmodes/RI5WDrive.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.ftc16072.Opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class RI5WDrive extends QQOpmode{
+    public void loop(){
+        super.loop();
+        //nav.driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        if(gamepad1.x){
+            robot.intake.intake();
+        }else{
+            robot.intake.stop();
+        }
+        if(gamepad1.a){
+            robot.intake.outtake();
+        }
+        int balls = robot.intake.numberOfBalls();
+        telemetry.addData("Number of Balls", balls);
+    }
+}

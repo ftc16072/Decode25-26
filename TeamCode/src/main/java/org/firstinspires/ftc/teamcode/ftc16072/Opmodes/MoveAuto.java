d3ae48840e85b4048a85373d08e9c64bf6df7dfa
Alan Smith <alan@randomsmiths.com>
2025-10-07 20:48:32 -0400

    Not working but to share

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Opmodes/MoveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Opmodes/MoveAuto.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.ftc16072.Opmodes;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
@@ -20,18 +21,19 @@ public class MoveAuto extends QQOpmode {
     public void init() {
         super.init();
         follower = Constants.createFollower(hardwareMap);
-
+        follower.setPose(new Pose(56, 9, Math.toRadians(90)));
         moveForward = follower.pathBuilder()
                 .addPath(
                         // Path 1
                         new BezierLine(new Pose(56.000, 9.000), new Pose(56.000, 30.000))
                 )
-                .setConstantHeadingInterpolation(Math.toRadians(270))
+                .setConstantHeadingInterpolation(Math.toRadians(90))
                 .build();
     }
     public void loop(){
         super.loop();
         follower.update();
+        telemetry.addData("Position", follower.getPose());
         switch(step){
             case 0:
                 follower.followPath(moveForward);
